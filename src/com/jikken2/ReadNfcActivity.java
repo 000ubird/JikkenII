870df654ede6d21bdfccf72ed4109660c2e20699
000ubird <000ubird@gmail.com>
2014-12-05 00:58:10 +0900

    byte型変数をString型の文字列に変換するメソッドを定義
    IDm情報を取得した時、byte型のデータをString文字列にする為に使用する

--- src/com/jikken2/ReadNfcActivity.java
+++ src/com/jikken2/ReadNfcActivity.java
@@ -9,5 +9,21 @@ public class ReadNfcActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_read_nfc);
+	/**
+	 * byteŒ^•Ï”‚ðStringŒ^‚Ì•¶Žš—ñ‚É•ÏŠ·‚·‚é
+	 * @param idm byteŒ^‚Ìidm•Ï”
+	 * @return StringŒ^‚Ìidm•¶Žš—ñ
+	 */
+	@SuppressLint("InlinedApi")
+	private String getIdm(byte[] idm) {
+		String buf = null;
+		StringBuffer idmByte = new StringBuffer();
+		if (idm != null) {
+			for (int i = 0; i < idm.length; i++) {
+				idmByte.append(Integer.toHexString(idm[i] & 0xff));
+			}
+			buf = idmByte.toString();
+		}
+		return buf;
 	}
 }
