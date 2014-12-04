3d6034fb17dcdca50d4a453246e0770def53cefe
000ubird <000ubird@gmail.com>
2014-12-05 01:07:26 +0900

    onCreateメソッドにNFC機能に関する処理を定義
    NFC機能が端末に搭載されていなかった場合、もしくはNFC機能がOFFになっている場合はトースト機能を使ってユーザに通知する
    その他、コメント文を追加した

--- src/com/jikken2/ReadNfcActivity.java
+++ src/com/jikken2/ReadNfcActivity.java
@@ -1,14 +1,44 @@
 package com.jikken2;
 
-import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
 
+@SuppressLint("NewApi")
 public class ReadNfcActivity extends ActionBarActivity {
-
+	protected NfcAdapter nfcAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//ƒX[ƒp[ƒNƒ‰ƒX‚ÌonCreateƒƒ\ƒbƒhŒÄ‚Ño‚µ
 		super.onCreate(savedInstanceState);
+		//ƒŒƒCƒAƒEƒgÝ’èƒtƒ@ƒCƒ‹‚ÌŽw’è
 		setContentView(R.layout.activity_read_nfc);
+		
+        // NFC‚ðˆµ‚¤‚½‚ß‚ÌƒCƒ“ƒXƒ^ƒ“ƒX‚ðŽæ“¾
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        // NFC‚ª“‹Ú‚³‚ê‚Ä‚¢‚é‚©ƒ`ƒFƒbƒN
+        if (nfcAdapter != null) {
+            // NFC‹@”\‚ª—LŒø‚É‚È‚Á‚Ä‚¢‚é‚©ƒ`ƒFƒbƒN
+            if (!nfcAdapter.isEnabled()) {
+                // NFC‹@”\‚ª–³Œø‚Ìê‡‚Íƒ†[ƒU[‚Ö’Ê’m
+                Toast.makeText(getApplicationContext(),
+                        "NFC‹@”\‚ª—LŒø‚É‚È‚Á‚Ä‚¢‚Ü‚¹‚ñ",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            // NFC”ñ“‹Ú‚Ìê‡‚Íƒ†[ƒU[‚Ö’Ê’m
+            Toast.makeText(getApplicationContext(),
+                    "NFC‹@”\‚ª”ñ“‹Ú‚Å‚·", Toast.LENGTH_SHORT)
+                    .show();
+        }
+	}
+	
 	/**
 	 * byteŒ^•Ï”‚ðStringŒ^‚Ì•¶Žš—ñ‚É•ÏŠ·‚·‚é
 	 * @param idm byteŒ^‚Ìidm•Ï”
