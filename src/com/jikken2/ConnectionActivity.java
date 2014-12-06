aade04c8126962b83fc103d659961603da367192
000ubird <000ubird@gmail.com>
2014-12-07 00:13:57 +0900

    データベースとのコネクションを行うアクティビティを定義

---
+++ src/com/jikken2/ConnectionActivity.java
@@ -0,0 +1,14 @@
+package com.jikken2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConnectionActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		//スーパークラスのonCreateメソッド呼び出し
+		super.onCreate(savedInstanceState);
+		//レイアウト設定ファイルの指定
+		setContentView(R.layout.activity_connection);
+	}
+}
