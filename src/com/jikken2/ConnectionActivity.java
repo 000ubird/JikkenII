502a7366d557e50de2c3f127a53a97db183b48c7
000ubird <000ubird@gmail.com>
2014-12-08 00:35:01 +0900

    接続アクティビティの更新
    ・前のアクティビティで取得したIDｍ情報をトースト表示する
    ・データベースとのコネクションを張る

--- src/com/jikken2/ConnectionActivity.java
+++ src/com/jikken2/ConnectionActivity.java
@@ -2,6 +2,8 @@ package com.jikken2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class ConnectionActivity extends Activity {
 	@Override
@@ -10,5 +12,13 @@ public class ConnectionActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		//レイアウト設定ファイルの指定
 		setContentView(R.layout.activity_connection);
+		
+		//IDm情報をトースト表示
+		Intent i = getIntent();
+		Toast.makeText(getApplicationContext(),
+			i.getStringExtra("ID"), Toast.LENGTH_SHORT)
+			.show();
+		ConnectDB cDB = new ConnectDB(ConnectionActivity.this);
+		cDB.execute();
 	}
 }
