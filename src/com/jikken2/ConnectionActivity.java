53033b0d5cabf2c864e46f98ca53dccc1371cafc
= <000ubird@gmail.com>
2014-12-22 17:16:07 +0900

    データベースとの接続を行うアクティビティの動作を変更
    ・データベースとの接続に失敗したらアラートダイアログを表示してNFC読み込みアクティビティに戻る
    ・接続に成功した場合、データを取得したタイミング(バックグラウンド処理が終了したタイミング)で取得したデータをトースト表示する
    
    Signed-off-by: = <000ubird@gmail.com>

--- src/com/jikken2/ConnectionActivity.java
+++ src/com/jikken2/ConnectionActivity.java
@@ -1,26 +1,63 @@
 package com.jikken2;
 
+import com.jikken2.ConnectDB.AsyncTaskCallback;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class ConnectionActivity extends Activity {
-	private static String sql = "SELECT id,pass FROM test";
-	
+public class ConnectionActivity extends Activity implements AsyncTaskCallback{
+	//private static String sql = "SELECT id,pass FROM test WHERE ";
+	private ConnectDB cDB;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		//スーパークラスのonCreateメソッド呼び出し
 		super.onCreate(savedInstanceState);
 		//レイアウト設定ファイルの指定
 		setContentView(R.layout.activity_connection);
-		
+
 		//IDm情報をトースト表示
 		Intent i = getIntent();
-		Toast.makeText(getApplicationContext(),
-			i.getStringExtra("ID"), Toast.LENGTH_SHORT)
-			.show();
-		ConnectDB cDB = new ConnectDB(ConnectionActivity.this,sql);
+		//sql = "INSERT into test(id,pass) values (\""+i.getStringExtra("ID")+"\",\"\");";
+		cDB = new ConnectDB(ConnectionActivity.this,"SELECT pass FROM test WHERE id = '"+i.getStringExtra("ID")+"';",this);
+		//cDB = new ConnectDB(ConnectionActivity.this,"SELECT pass FROM test WHERE id = '"+"user3"+"';",this);
 		cDB.execute();
 	}
+
+	public void preExecute() {
+		//だいたいの場合ダイアログの表示などを行う
+	}
+	public void postExecute(String result) {
+		//AsyncTaskの結果を受け取ってなんかする
+		String r = cDB.getResult();
+		if(r.equals("接続エラーです")){
+			new AlertDialog.Builder(ConnectionActivity.this)
+			.setTitle("接続エラーです。NFC読み取り画面に移行します。")
+			.setNegativeButton(
+					"はい", 
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which){
+							finish();
+						}
+					})
+					.setCancelable(false)	//ダイアログ以外の場所のタッチは無効
+					.show();
+		}
+		else {
+			Toast.makeText(getApplicationContext(),
+					"通信成功→"+r, Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+	public void progressUpdate(int progress) {
+		//だいたいプログレスダイアログを進める
+	}
+	public void cancel() {
+		//キャンセルされた時になんかする
+	}
+
 }
