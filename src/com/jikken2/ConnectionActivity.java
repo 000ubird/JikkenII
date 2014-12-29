528c98d7c4a27eb48186aeac3a522914785c8784
000ubird <000ubird@gmail.com>
2014-12-30 02:41:00 +0900

    接続アクティビティのコードの整形とレイアウトファイルの更新

--- src/com/jikken2/ConnectionActivity.java
+++ src/com/jikken2/ConnectionActivity.java
@@ -7,82 +7,74 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.widget.Toast;
 
 public class ConnectionActivity extends Activity implements AsyncTaskCallback{
-	//private static String sql = "SELECT id,pass FROM test WHERE ";
 	private ConnectDB cDB;
+	private String id = "";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		//スーパークラスのonCreateメソッド呼び出し
 		super.onCreate(savedInstanceState);
 		//レイアウト設定ファイルの指定
 		setContentView(R.layout.activity_connection);
-
-		//IDm情報をトースト表示
+		
+		//前のアクティビティからID情報を取得
 		Intent i = getIntent();
-		//sql = "INSERT into test(id,pass) values (\""+i.getStringExtra("ID")+"\",\"\");";
-		cDB = new ConnectDB(ConnectionActivity.this,"SELECT pass FROM test WHERE id = '"+i.getStringExtra("ID")+"';",this);
-		//cDB = new ConnectDB(ConnectionActivity.this,"SELECT pass FROM test WHERE id = '"+"user3"+"';",this);
-		cDB.execute();
+		id = i.getStringExtra("ID");
+		
+		//SQL文を実行
+		cDB = new ConnectDB(ConnectionActivity.this,"SELECT pass FROM test WHERE id = '"+id+"';",this);
+		//sql = "INSERT into test(id,pass) values (\""+i.getStringExtra("ID")+"\",\"\");";	//デバッグ
+		//cDB = new ConnectDB(ConnectionActivity.this,"SELECT pass FROM test WHERE id = '"+"user3"+"';",this);	//デバッグ
+		cDB.execute();	//バックグラウンド処理開始
 	}
 
-	public void preExecute() {
-		//だいたいの場合ダイアログの表示などを行う
-	}
+	public void preExecute() { }
+	//バックグラウンド処理完了後の処理
 	public void postExecute(String result) {
-		//AsyncTaskの結果を受け取ってなんかする
 		String r = cDB.getResult();
 		if(r.equals("接続エラーです")){
 			new AlertDialog.Builder(ConnectionActivity.this)
 			.setTitle("接続エラーです。NFC読み取り画面に移行します。")
-			.setNegativeButton(
-					"はい", 
-					new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int which){
-							finish();
-						}
-					})
-					.setCancelable(false)	//ダイアログ以外の場所のタッチは無効
-					.show();
+			.setCancelable(false)	//ダイアログ以外の場所のタッチは無効
+			.setNegativeButton("はい", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which){
+					//NFC読み取りアクティビティに遷移
+		            Intent i = new Intent(ConnectionActivity.this,ReadNfcActivity.class);
+		            startActivity(i);
+				}
+			}).show();
 		}
 		else {
 			//パスワード情報が無かった場合はパスワード登録画面に遷移
 			if(r.equals("")){
 				new AlertDialog.Builder(ConnectionActivity.this)
 				.setTitle("パスワードが登録されていません。パスワード登録画面に移行します。")
+				.setCancelable(false)	//ダイアログ以外の場所のタッチは無効
 				.setNegativeButton("はい", new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which){
 			            Intent i = new Intent(ConnectionActivity.this,SignUp.class);
 			            startActivity(i);
 					}
-				})
-				.setCancelable(false)	//ダイアログ以外の場所のタッチは無効
-				.show();
+				}).show();
 			}
 			//パスワード情報が有った場合はログイン画面に遷移
 			else{
 				new AlertDialog.Builder(ConnectionActivity.this)
 				.setTitle("ログイン画面に移行します。")
+				.setCancelable(false)	//ダイアログ以外の場所のタッチは無効
 				.setNegativeButton("はい", new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which){
 			            Intent i = new Intent(ConnectionActivity.this,SignUp.class);
 			            startActivity(i);
 					}
-				})
-				.setCancelable(false)	//ダイアログ以外の場所のタッチは無効
-				.show();
+				}).show();
 			}
 		}
 	}
-	public void progressUpdate(int progress) {
-		//だいたいプログレスダイアログを進める
-	}
-	public void cancel() {
-		//キャンセルされた時になんかする
-	}
-
+	public void progressUpdate(int progress) { }
+	public void cancel() { }
 }
