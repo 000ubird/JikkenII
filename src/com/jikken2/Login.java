da1e4ae514aa9d6e5feb4987682bb53531e9fb75
000ubird <000ubird@gmail.com>
2014-12-30 03:45:22 +0900

    ログインボタンを押した際の処理を実装
    パスワードが一致したらトップ画面(実装予定)に遷移
    パスワードが一致しなかったら再入力を求める

--- src/com/jikken2/Login.java
+++ src/com/jikken2/Login.java
@@ -1,11 +1,14 @@
 package com.jikken2;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,7 +39,24 @@ public class Login extends Activity {
 	class ButtonClickListener implements OnClickListener{
 		@Override
 		public void onClick(View v) {
-			Toast.makeText(Login.this, pass, Toast.LENGTH_LONG).show();
+			EditText editText1 = (EditText)findViewById(R.id.editText1);
+			String password = editText1.getText().toString();
+			
+			if(password.equals(pass)){
+				new AlertDialog.Builder(Login.this)
+				.setTitle("ログイン成功")
+				.setCancelable(false)	//ダイアログ以外の場所のタッチは無効
+				.setNegativeButton("やったぜ！",new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which){
+			            Intent i = new Intent(Login.this,ReadNfcActivity.class);
+			            startActivity(i);
+					}
+				}).show();
+			}
+			else{
+				Toast.makeText(Login.this, "パスワードが正しくありません。正しいパスワードは"+pass+"ですよ", Toast.LENGTH_LONG).show();	
+			}
 		}
 	}
 }
